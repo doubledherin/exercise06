adfed7829fca9ff35889818bf196beb29982c2d0
Wendy Dherin <wendydherin@gmail.com>
2014-10-06 19:53:59 -0700

    Implemented using dictionary instead.

--- sorted_data.py
+++ sorted_data.py
@@ -2,28 +2,18 @@ from sys import argv
 
 script, filename = argv
 
-restaurant_raw = open(filename)
+fin = open(filename)
 
-restos = []
-scores = []
+resto_ratings = {}
 
-for line in restaurant_raw:
+for line in fin:
     line = line.rstrip().split(":")
-    restos.append(line[0]) 
-    scores.append(line[1])
+    resto_ratings[line[0]] = line[1]
 
-# print restos
-# print scores
+print resto_ratings
 
 
-rest_scores = zip(restos,scores)
-
-
-sorted_rest_scores = sorted(rest_scores)
-
-print sorted_rest_scores
-
-for item in sorted_rest_scores:
-    print "Restaurant %s is rated at %s." % (item[0], item[1])
+#for item in sorted_rest_scores:
+ #   print "Restaurant %s is rated at %s." % (item[0], item[1])
 
     
